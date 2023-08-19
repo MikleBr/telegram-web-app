6cadeeadcb679ceb72483713130dfca19d210258
mikhail <barulin.ma@gmail.com>
2023-08-19 23:49:37 +0300

    feature: Render telegram data

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 function App() {
   // tg.initDataUnsafe.user.first_name
-  const clientPhoto = '';
+  const telegramUser = window.Telegram.WebApp.initDataUnsafe.user;
 
   return (
     <div className="bg-white flex flex-col items-center h-full max-w-[420px] w-[calc(100%-32px)] mx-auto">
       <h1 className="text-xl font-bold mt-2">Профиль</h1>
       <div>
-        {clientPhoto && (
+        {telegramUser.photo_url && (
           <img
             className="w-8 h-8 rounded-full object-cover"
-            src={clientPhoto}
+            src={telegramUser.photo_url}
           />
         )}
-        <p className="font-medium mt-2">Михаил Барулин</p>
+        <p className="font-medium mt-2">
+          {telegramUser.first_name} {telegramUser.last_name}
+        </p>
       </div>
 
       <div className="w-full mt-4 gap-2 flex items-center justify-between">
